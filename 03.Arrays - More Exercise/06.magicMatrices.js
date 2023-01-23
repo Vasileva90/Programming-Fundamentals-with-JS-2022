b79c7d0808655351c9028086aa332a8f51e35797
Vasileva90 <107758821+Vasileva90@users.noreply.github.com>
2023-01-23 12:43:58 +0200

    Add tasks

---
+++ 03.Arrays - More Exercise/06.magicMatrices.js
@@ -0,0 +1,21 @@
+function magicMatrices(matrix) {
+    let sum = matrix[0].reduce((sum, x) => sum + x);
+    for (let row = 0; row < matrix.length; row++) {
+        let rowSum = 0;
+        let colSum = 0;
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (col < matrix.length) {
+                rowSum += matrix[col][row];
+            }           
+            colSum += matrix[row][col];
+        }
+        if (rowSum != sum || colSum != sum) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(magicMatrices([[4, 5, 6],
+                           [6, 5, 4],
+                           [5, 5, 5]]));
